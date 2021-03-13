7098c9700c40d507475239d557a8f027039749b2
Marike Elwers <m.e.elwers@gmail.com>
2021-03-13 14:13:53 +0100

    resolved conflicts in stylesheet and index

--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-<<<<<<< HEAD
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
-=======
 import App from './components/app';
 
 const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
->>>>>>> ee532196bbdadc160498f4b48358d87709cbf06b
